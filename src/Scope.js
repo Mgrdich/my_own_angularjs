d94c4046cd0cdf1c7b1d7664f7b66f96ea2974f1
Mgrdich <mgotm13@gmail.com>
2020-02-18 01:27:44 +0400

    adding $watch and $digest into the scope

--- src/Scope.js
+++ src/Scope.js
@@ -1,3 +1,23 @@
-module.exports = function Scope() {
+function Scope() {
+    this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function (watchFn, listenerFn) {
+
+    let watcher = {
+        watchFn: watchFn,
+        listenerFn: listenerFn
+    };
+
+    this.$$watchers.push(watcher);
 
 };
+
+Scope.prototype.$digest = function () {
+    _.forEach(this.$$watchers, function (watcher) {
+        watcher.listenerFn();
+    });
+};
+
+
+module.exports = {Scope};
--- test/Scope.test.js
+++ test/Scope.test.js
@@ -1,7 +1,37 @@
-let scope = require("../src/Scope");
-describe("Hello", function () {
-    it("says hello", function () {
-        // expect(scope()).toBe(1);
+let Scope = require("../src/Scope");
+describe("Scope", function () {
+
+    it("can be constructed using an object", function () {
+        let scope = new Scope();
+        scope.aProperty = 1;
+        expect(scope.aProperty).toBe(1);
     });
+
+
+    let scope;
+
+    beforeEach(function () {
+        scope = new Scope();
+    });
+
+    it("calls the listener function of a watch o first $digest",function () {
+
+        let watchFn  = function () {
+            return 'wat';
+        };
+        let listenerFn = jest.fn();
+
+        jest.spyOn(watchFn,'listenerFn');
+
+        scope.$watch(watchFn,listenerFn);
+
+        scope.$digest();
+
+        expect(listenerFn).toHaveBeenCalled();
+
+    });
+
+
+
 });
 
