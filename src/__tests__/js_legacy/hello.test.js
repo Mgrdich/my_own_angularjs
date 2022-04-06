215ba7e738fd67528f16e4ef2a5200e95b7078e3
Mgrdich <mgotm13@gmail.com>
2022-04-06 23:11:47 +0400

    Add eslint Config + prettier config

--- src/__tests__/js_legacy/hello.test.js
+++ src/__tests__/js_legacy/hello.test.js
@@ -1,7 +1,8 @@
-let sayHello = require("js_legacy/hello");
-describe("Hello", function () {
-    it("says hello", function () {
-         expect(sayHello()).toBe("Hello, world!");
-    });
-});
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+let sayHello = require('js_legacy/hello');
 
+describe('Hello', function () {
+  it('says hello', function () {
+    expect(sayHello()).toBe('Hello, world!');
+  });
+});
